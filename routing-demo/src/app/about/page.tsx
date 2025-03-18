8272b682cf5f869896df40ed5c966b8334b68bb7
abhiram-ar <abhiram2001.ars@gmail.com>
2025-03-18 14:33:04 +0530

    layout, metadata

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,11 @@
-export default function About(){
-    return <h1>about page</h1>
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "I am free about",
+  },
+};
+
+export default function About() {
+  return <h1>about page</h1>;
 }
--- routing-demo/src/app/product/[productId]/page.tsx
+++ routing-demo/src/app/product/[productId]/page.tsx
@@ -1,8 +1,22 @@
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) =>
+    setTimeout(() => resolve(`iphone ${id}`), 5 * 1000)
+  );
+  return {
+    title: `product: ${title}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Product Details {productId}</h1>;
 }
